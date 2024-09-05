3e3705c812f1666880f7438f7da0bde61a1cba34
pep020501 <44673825+pep020501@users.noreply.github.com>
2024-09-05 13:57:14 -0400

    Add files via upload

---
+++ Wordreverse.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wordreverse;
+
+import java.util.ArrayList;
+
+public class Wordreverse {
+
+  private static String word = "Marsha is a Trumpie";
+  private static String reverseword ="";
+  private static char letter;
+    public static void main(String[] args) {
+        System.out.println("Before: " +word);
+        for (int i = 0; i < word.length(); i++){
+            letter = word.charAt(i);
+            reverseword = letter + reverseword.toLowerCase();
+        }
+            System.out.println("After: "+reverseword);
+            System.out.println(word.substring(0, 6));
+            System.out.println(word.equals(reverseword));
+    }
+    
+}
